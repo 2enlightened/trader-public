5075df67ff2945bb02a94e57c1d6e7a7306764a8
xficek05 <94891449+xficek05@users.noreply.github.com>
2022-02-11 19:42:32 +0100

    Update trader.py

--- trader.py
+++ trader.py
@@ -1,17 +1,16 @@
-import os
-
 from binance.client import Client
 
 # init
 api_key = 'geM8dY8xZG02nYuFVVpay2asDhGMhJn24sKTgLLH5isIBGtiAC5pp5pJ2CZIlHnp'
 api_secret = 'geM8dY8xZG02nYuFVVpay2asDhGMhJn24sKTgLLH5isIBGtiAC5pp5pJ2CZIlHnp'
 client = Client(api_key, api_secret)
-
+i = 0
 ## main
-
+while i <= 10:
 # get latest price from Binance API
-btc_price = client.get_symbol_ticker(symbol='BTCUSDT')
+    btc_price = client.get_symbol_ticker(symbol='BTCUSDT')
 # print full output (dictionary)
-print(btc_price)
+    print(btc_price)
 # print just the price
-print(btc_price['price'])
+    print(btc_price['price'])
+    i+=1
