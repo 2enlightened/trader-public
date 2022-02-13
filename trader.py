1e8bf5771eb9bfbd4b82b25589bc064981237662
xficek05 <94891449+xficek05@users.noreply.github.com>
2022-02-13 11:57:02 +0100

    Update trader.py

--- trader.py
+++ trader.py
@@ -1,16 +1,20 @@
 from binance.client import Client
 
 # init
-api_key = ''
-api_secret = ''
-client = Client(api_key, api_secret)
-i = 0
+api_key_Binance = 'geM8dY8xZG02nYuFVVpay2asDhGMhJn24sKTgLLH5isIBGtiAC5pp5pJ2CZIlHnp'
+api_secret_Binance = 'geM8dY8xZG02nYuFVVpay2asDhGMhJn24sKTgLLH5isIBGtiAC5pp5pJ2CZIlHnp'
+
+#api_key_Coinbase = '1qC6BQIj2zpN3Eou'
+#api_secret_Coinbase = 'TJghP58m2smAYXim5G4miXCF3rG2pxms'
+
+client = Client(api_key_Binance, api_secret_Binance)
+t = TRUE
 ## main
-while i <= 10:
-# get latest price from Binance API
+while t:
+    ## get lates data Binance
     btc_price = client.get_symbol_ticker(symbol='BTCUSDT')
-# print full output (dictionary)
-    print(btc_price)
+    eth_price = client.get_symbol_ticker(symbol='ETHUSDT')
+    btc = btc_price['price']
+    eth = eth_price['price']
 # print just the price
-    print(btc_price['price'])
-    i+=1
+    print("BTC: ",btc, "ETH: ",eth)
