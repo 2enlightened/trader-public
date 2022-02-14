3ae48e746812e774427e0d2ed7b533452a16dba2
xficek05 <94891449+xficek05@users.noreply.github.com>
2022-02-14 01:11:12 +0100

    Update trader.py

--- trader.py
+++ trader.py
@@ -1,4 +1,18 @@
 from binance.client import Client
+import pandas as pd
+import btalib
+import numpy as np
+import time
+
+def getminutedata(symbol, interval, lookback):
+    frame = pd.DataFrame(client.get_historical_klines(symbol, interval, lookback + 'min ago UTC'))
+    frame = frame.iloc[:,:6]
+    frame.columns = ['Time','Opent','High','Low','Close','Volume']
+    frame = frame.set_index('Time')
+    frame.index = pd.to_datetime(frame.index, unit="ms")
+    frame = frame.astype(float)
+    return frame
+
 
 # binance api
 api_key_Binance = 'geM8dY8xZG02nYuFVVpay2asDhGMhJn24sKTgLLH5isIBGtiAC5pp5pJ2CZIlHnp'
@@ -8,7 +22,9 @@ api_secret_Binance = 'geM8dY8xZG02nYuFVVpay2asDhGMhJn24sKTgLLH5isIBGtiAC5pp5pJ2C
 #api_secret_Coinbase = 'TJghP58m2smAYXim5G4miXCF3rG2pxms'
 
 client = Client(api_key_Binance, api_secret_Binance)
+print('\x1bc')
 pair = input("Zadej pair napr BTCUSDT: ")
+
 t = True
 ## main
 while t:
@@ -16,8 +32,14 @@ while t:
     data = client.get_ticker(symbol=pair)
     price = data['lastPrice']
     avg_price = data['weightedAvgPrice']
-    data = pd.DataFrame({'symbol': pair, 'price': data['lastPrice'], 'AVG': data['weightedAvgPrice'],'priceChange': data['priceChange'], '%': data['priceChangePercent'], 'LOW': data['lowPrice'], 'HIGH': data['highPrice'], 'VOLUME': data['volume'] }, index=[0])
-    if price > avg_price:
+    percent = float(data['priceChangePercent'])
+    df = getminutedata(pair,'1m','100')
+    rsi = btalib.rsi(df, period=14)
+    df = df.join([rsi.df])
+    RSI = rsi.df.rsi[-1]
+    data = pd.DataFrame({'symbol': pair, 'price': data['lastPrice'], 'AVG': data['weightedAvgPrice'],'priceChange': data['priceChange'], '%': percent, 'RSI': RSI}, index=[0])
+    
+    if percent >= 0:
         print("\033[32m {} \033[0m".format(data))
     else:
         print("\033[31m {} \033[40m".format(data))  
